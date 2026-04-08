736eeca3e126b07b1366a59ad41e7ad1eefcd2b1
Sungho255 <kkb053404@gmail.com>
2026-04-08 15:13:45 +0900

    feat: Quiz Menu

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 import json
 
-menu_list = ["퀴즈 풀기","퀴즈 추가","퀴즈 목록","점수 확인","종료"]
 
 @dataclass
 class Quiz:
@@ -20,6 +19,7 @@ class QuizGame:
     select_num: int = 0
 
     def play_quiz(self):
+        
         return True
 
     def add_quiz(self):
@@ -35,6 +35,16 @@ class QuizGame:
         return True
 
 if __name__ == "__main__":
+    menu_dict = {"퀴즈 풀기": None, "퀴즈 추가": None, "퀴즈 목록": None, "점수 확인": None, "종료": None}
+
     print('='*30)
     print('🎲나만의 퀴즈 게임🎲')
     print('='*30)
+
+    for i, menu in enumerate(menu_dict.keys()):
+        print(f"{i+1}. {menu}")
+    print('='*30)
+
+    input_num = int(input("메뉴를 선택하세요: "))
+    
+    print(list(menu_dict.keys())[input_num-1])
